c02fdd138eccefc002963ef932a3e91adaf6c214
KSentak <k.sentak@outlook.com>
2020-01-17 12:25:46 -0500

    Removed navbar

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,8 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
 import { setCurrentUser, logoutUser } from "./actions/authActions";
-
 import { Provider } from "react-redux";
 import store from "./store";
-
-import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
@@ -41,7 +38,6 @@ class App extends Component {
 
 				<Router>
 					<div className="App">
-						<Navbar />
 						<Route exact path="/" component={Landing} />
 						<Route exact path="/register" component={Register} />
 						<Route exact path="/login" component={Login} />
