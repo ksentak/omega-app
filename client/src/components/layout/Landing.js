013c71014b8298ca2d27fdc965ef88776bfd7472
KSentak <k.sentak@outlook.com>
2020-01-17 16:41:05 -0500

    Styling of login and register buttons

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -7,18 +7,18 @@ class Landing extends Component {
 		return (
 			<div className="container col-12 m-0 p-0">
 				<div className="jumbotron jumbotron-fluid">
-					<h4 className="text-center">Build a React application with user authentication</h4>
-					<h6 className="text-center">via JWTs and passport</h6>
+					<h2 className="text-center">Build a React application with user authentication</h2>
+					<h5 className="text-center">via JWTs and passport</h5>
 				</div>
 
 				{/* Links to Register & Login */}
 				<div className="row">
 					<div className="col-sm-10 offset-1 text-center">
 						{/* Register */}
-						<Link to="/register" className="btn bg-success">Register</Link>
+						<Link to="/register" className="btn registerButton">Register</Link>
 
 						{/* Login */}
-						<Link to="/login" className="btn bg-primary">Login</Link>
+						<Link to="/login" className="btn loginButton">Login</Link>
 					</div>
 				</div>
 			</div>
