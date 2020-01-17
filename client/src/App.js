399a8930939e486dd225ff1ab6981b9bdd31b57c
KSentak <k.sentak@outlook.com>
2020-01-17 12:03:11 -0500

    Reformatted using tabs

--- client/src/App.js
+++ client/src/App.js
@@ -16,43 +16,43 @@ import Dashboard from "./components/dashboard/Dashboard";
 
 // Check for token to keep user logged in
 if (localStorage.jwtToken) {
-  // Set auth token header auth
-  const token = localStorage.jwtToken;
-  setAuthToken(token);
-  // Decode token and get user info and exp
-  const decoded = jwt_decode(token);
-  // Set user and isAuthenticated
-  store.dispatch(setCurrentUser(decoded));
-  // Check for expired token
-  const currentTime = Date.now() / 1000; // to get in milliseconds
+	// Set auth token header auth
+	const token = localStorage.jwtToken;
+	setAuthToken(token);
+	// Decode token and get user info and exp
+	const decoded = jwt_decode(token);
+	// Set user and isAuthenticated
+	store.dispatch(setCurrentUser(decoded));
+	// Check for expired token
+	const currentTime = Date.now() / 1000; // to get in milliseconds
 
-  if (decoded.exp < currentTime) {
-    // Logout user
-    store.dispatch(logoutUser());
-    // Redirect to login
-    window.location.href = "./login";
-  }
+	if (decoded.exp < currentTime) {
+		// Logout user
+		store.dispatch(logoutUser());
+		// Redirect to login
+		window.location.href = "./login";
+	}
 }
 
 class App extends Component {
-  render() {
-    return (
-      <Provider store={store}>
+	render() {
+		return (
+			<Provider store={store}>
 
-        <Router>
-          <div className="App">
-            <Navbar />
-            <Route exact path="/" component={Landing} />
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
-            <Switch>
-              <PrivateRoute exact path="/dashboard" component={Dashboard} />
-            </Switch>
-          </div>
-        </Router>
-      </Provider>
+				<Router>
+					<div className="App">
+						<Navbar />
+						<Route exact path="/" component={Landing} />
+						<Route exact path="/register" component={Register} />
+						<Route exact path="/login" component={Login} />
+						<Switch>
+							<PrivateRoute exact path="/dashboard" component={Dashboard} />
+						</Switch>
+					</div>
+				</Router>
+			</Provider>
 
-    );
-  }
+		);
+	}
 }
 export default App;
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -3,52 +3,52 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
 class Dashboard extends Component {
-  onLogoutClick = e => {
-    e.preventDefault();
-    this.props.logoutUser();
-  };
-render() {
-    const { user } = this.props.auth;
-return (
-      <div style={{ height: "75vh" }} className="container">
-        <div className="row">
-          <div className="col-sm-12 text-center">
-            <h4>
-              <b>Hey there,</b> {user.name.split(" ")[0]}
-              <p className="danger">
-                You are logged into a full-stack{" "}
-                <span>MERN</span> app 👏
+	onLogoutClick = e => {
+		e.preventDefault();
+		this.props.logoutUser();
+	};
+	render() {
+		const { user } = this.props.auth;
+		return (
+			<div style={{ height: "75vh" }} className="container">
+				<div className="row">
+					<div className="col-sm-12 text-center">
+						<h4>
+							<b>Hey there,</b> {user.name.split(" ")[0]}
+							<p className="danger">
+								You are logged into a full-stack{" "}
+								<span>MERN</span> app 👏
               </p>
-            </h4>
-            <button
-              style={{
-                width: "150px",
-                borderRadius: "3px",
-                letterSpacing: "1.5px",
-                marginTop: "1rem"
-              }}
-              onClick={this.onLogoutClick}
-              className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-            >
-              Logout
+						</h4>
+						<button
+							style={{
+								width: "150px",
+								borderRadius: "3px",
+								letterSpacing: "1.5px",
+								marginTop: "1rem"
+							}}
+							onClick={this.onLogoutClick}
+							className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+						>
+							Logout
             </button>
-          </div>
-        </div>
-      </div>
-    );
-  }
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 Dashboard.propTypes = {
-  logoutUser: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired
+	logoutUser: PropTypes.func.isRequired,
+	auth: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
-  auth: state.auth
+	auth: state.auth
 });
 
 export default connect(
-  mapStateToProps,
-  { logoutUser }
+	mapStateToProps,
+	{ logoutUser }
 )(Dashboard);
