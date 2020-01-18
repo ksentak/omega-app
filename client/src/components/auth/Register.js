0a0130fe8e6f80a16cb35a41d2875b6b69848762
KSentak <k.sentak@outlook.com>
2020-01-18 12:43:34 -0500

    Linked font awesome to the project

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -53,98 +53,106 @@ class Register extends Component {
 		return (
 			<div className="container">
 				<div className="row">
-					<div className="col-sm-10 offset-2">
-						<Link to="/" className="btn-danger">
-							<i className="material-icons left">keyboard_backspace</i> Back to
-							home
-			  </Link>
-						<div className="col-sm-12" style={{ paddingLeft: "11.250px" }}>
-							<h4>
-								<b>Register</b> below
-				</h4>
-							<p className="text-danger">
-								Already have an account? <Link to="/login">Log in</Link>
-							</p>
-						</div>
-						<form noValidate onSubmit={this.onSubmit}>
-							<div className="input-field col-sm-12">
-								<input
-									onChange={this.onChange}
-									value={this.state.name}
-									error={errors.name}
-									id="name"
-									type="text"
-									className={classnames("", {
-										invalid: errors.name
-									})}
-								/>
-								<label htmlFor="name">Name</label>
-								<span className="red-text">{errors.name}</span>
-
-							</div>
-							<div className="input-field col-sm-12">
-								<input
-									onChange={this.onChange}
-									value={this.state.email}
-									error={errors.email}
-									id="email"
-									type="email"
-									className={classnames("", {
-										invalid: errors.email
-									})}
-								/>
-								<label htmlFor="email">Email</label>
-								<span className="red-text">{errors.email}</span>
-
-							</div>
-							<div className="input-field col-sm-12">
-								<input
-									onChange={this.onChange}
-									value={this.state.password}
-									error={errors.password}
-									id="password"
-									type="password"
-									className={classnames("", {
-										invalid: errors.password
-									})}
-								/>
-								<label htmlFor="password">Password</label>
-								<span className="red-text">{errors.password}</span>
-
-							</div>
-							<div className="input-field col-sm-12">
-								<input
-									onChange={this.onChange}
-									value={this.state.password2}
-									error={errors.password2}
-									id="password2"
-									type="password"
-									className={classnames("", {
-										invalid: errors.password2
-									})}
-								/>
-								<label htmlFor="password2">Confirm Password</label>
-								<span className="red-text">{errors.password2}</span>
-
-							</div>
-							<div className="col-sm-12" style={{ paddingLeft: "11.250px" }}>
-								<button
-									style={{
-										width: "150px",
-										borderRadius: "3px",
-										letterSpacing: "1.5px",
-										marginTop: "1rem"
-									}}
-									type="submit"
-									className="btn btn-large"
-								>
-									Sign up
-				  </button>
-							</div>
-						</form>
+					<div className="col-sm-10 offset-1 text-center returnHome">
+						<Link to="/">Return Home</Link>
 					</div>
 				</div>
 			</div>
+
+			// <div className="container">
+			// 	<div className="row">
+			// 		<div className="col-sm-10 offset-2">
+			// 			<Link to="/" className="btn-danger">
+			// 				<i className="material-icons left">keyboard_backspace</i> Back to
+			// 				home
+			//   </Link>
+			// 			<div className="col-sm-12" style={{ paddingLeft: "11.250px" }}>
+			// 				<h4>
+			// 					<b>Register</b> below
+			// 	</h4>
+			// 				<p className="text-danger">
+			// 					Already have an account? <Link to="/login">Log in</Link>
+			// 				</p>
+			// 			</div>
+			// 			<form noValidate onSubmit={this.onSubmit}>
+			// 				<div className="input-field col-sm-12">
+			// 					<input
+			// 						onChange={this.onChange}
+			// 						value={this.state.name}
+			// 						error={errors.name}
+			// 						id="name"
+			// 						type="text"
+			// 						className={classnames("", {
+			// 							invalid: errors.name
+			// 						})}
+			// 					/>
+			// 					<label htmlFor="name">Name</label>
+			// 					<span className="red-text">{errors.name}</span>
+
+			// 				</div>
+			// 				<div className="input-field col-sm-12">
+			// 					<input
+			// 						onChange={this.onChange}
+			// 						value={this.state.email}
+			// 						error={errors.email}
+			// 						id="email"
+			// 						type="email"
+			// 						className={classnames("", {
+			// 							invalid: errors.email
+			// 						})}
+			// 					/>
+			// 					<label htmlFor="email">Email</label>
+			// 					<span className="red-text">{errors.email}</span>
+
+			// 				</div>
+			// 				<div className="input-field col-sm-12">
+			// 					<input
+			// 						onChange={this.onChange}
+			// 						value={this.state.password}
+			// 						error={errors.password}
+			// 						id="password"
+			// 						type="password"
+			// 						className={classnames("", {
+			// 							invalid: errors.password
+			// 						})}
+			// 					/>
+			// 					<label htmlFor="password">Password</label>
+			// 					<span className="red-text">{errors.password}</span>
+
+			// 				</div>
+			// 				<div className="input-field col-sm-12">
+			// 					<input
+			// 						onChange={this.onChange}
+			// 						value={this.state.password2}
+			// 						error={errors.password2}
+			// 						id="password2"
+			// 						type="password"
+			// 						className={classnames("", {
+			// 							invalid: errors.password2
+			// 						})}
+			// 					/>
+			// 					<label htmlFor="password2">Confirm Password</label>
+			// 					<span className="red-text">{errors.password2}</span>
+
+			// 				</div>
+			// 				<div className="col-sm-12" style={{ paddingLeft: "11.250px" }}>
+			// 					<button
+			// 						style={{
+			// 							width: "150px",
+			// 							borderRadius: "3px",
+			// 							letterSpacing: "1.5px",
+			// 							marginTop: "1rem"
+			// 						}}
+			// 						type="submit"
+			// 						className="btn btn-large"
+			// 					>
+			// 						Sign up
+			// 	  </button>
+			// 				</div>
+			// 			</form>
+			// 		</div>
+			// 	</div>
+			// </div>
 		);
 	}
 }
@@ -160,7 +168,4 @@ const mapStateToProps = state => ({
 	errors: state.errors
 });
 
-export default connect(
-	mapStateToProps,
-	{ registerUser }
-)(withRouter(Register));
+export default connect(mapStateToProps,{ registerUser }) (withRouter(Register));
