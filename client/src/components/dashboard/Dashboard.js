ef93aad38b5ce8353f69f3e0e656b05d8cf2220f
KSentak <k.sentak@outlook.com>
2020-01-18 20:51:50 -0500

    Added custom css to dashboard page

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
+import './style.css';
+
 class Dashboard extends Component {
 	onLogoutClick = e => {
 		e.preventDefault();
@@ -10,28 +12,12 @@ class Dashboard extends Component {
 	render() {
 		const { user } = this.props.auth;
 		return (
-			<div style={{ height: "75vh" }} className="container">
+			<div className="container">
 				<div className="row">
-					<div className="col-sm-12 text-center">
-						<h4>
-							<b>Hey there,</b> {user.name.split(" ")[0]}
-							<p className="danger">
-								You are logged into a full-stack{" "}
-								<span>MERN</span> app 👏
-              </p>
-						</h4>
-						<button
-							style={{
-								width: "150px",
-								borderRadius: "3px",
-								letterSpacing: "1.5px",
-								marginTop: "1rem"
-							}}
-							onClick={this.onLogoutClick}
-							className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-						>
-							Logout
-            </button>
+					<div className="col-sm-12 text-center dashboardText">
+						<h3>Hey there, {user.name.split(" ")[0]}</h3>
+						<h3>You are logged into a full-stack MERN app!</h3>
+						<button onClick={this.onLogoutClick} className="btn logoutbtn">Logout</button>
 					</div>
 				</div>
 			</div>
@@ -48,7 +34,4 @@ const mapStateToProps = state => ({
 	auth: state.auth
 });
 
-export default connect(
-	mapStateToProps,
-	{ logoutUser }
-	)(Dashboard);
+export default connect(mapStateToProps, { logoutUser })(Dashboard);
